f8e12753c24ee0324e8b882fad355fae07854eed
Kate Aplin <kaplin@hmc.edu>
2014-09-04 20:44:57 -0700

    Finished part 6 (through page 82, up to polishing the game)

--- BullsEye/BullsEye/KAViewController.m
+++ BullsEye/BullsEye/KAViewController.m
@@ -16,6 +16,8 @@
 {
     int _currentValue;
     int _targetValue;
+    int _score;
+    int _round;
 }
 
 - (void)viewDidLoad
@@ -28,6 +30,7 @@
 
 - (void)startNewRound
 {
+    _round += 1;
     _targetValue = 1 + arc4random_uniform(100);
     _currentValue = 50;
     self.slider.value = _currentValue;
@@ -36,6 +39,8 @@
 - (void)updateLabels
 {
     self.targetLabel.text = [NSString stringWithFormat:@"%d", _targetValue];
+    self.scoreLabel.text = [NSString stringWithFormat:@"%d", _score];
+    self.roundLabel.text = [NSString stringWithFormat:@"%d", _round];
 }
 
 - (void)didReceiveMemoryWarning
@@ -46,9 +51,12 @@
 
 - (IBAction)showAlert
 {
+    int difference = abs(_targetValue - _currentValue);
+    int points = 100 - difference;
+    _score += points;
+    
     NSString *message = [NSString stringWithFormat:
-                        @"The value of the slider is: %d\nThe target value is: %d",
-                         _currentValue, _targetValue];
+                        @"You scored %d points", points];
     
     UIAlertView *alertView = [[UIAlertView alloc]
                               initWithTitle:@"Hello World"
